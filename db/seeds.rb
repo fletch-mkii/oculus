be7be58ec47966ec879ea9877f55ab8ef5948a9e
Mark Fletcher <mook.fletcher@gmail.com>
2016-03-02 19:24:50 -0500

    games placeholder content set

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'date'
+
+10.times do |i|
+  Game.create(
+              team_one: "Hypernerds#{i}",
+              team_two: "Turbodweebs#{i}",
+              start_time: DateTime.now,
+              game_length: 37
+              )
+  sleep 1
+end
